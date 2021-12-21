aa9d0bef62c026be01f402e3e124a4172fffcdc0
ngong6 <ngong6@gatech.edu>
2021-12-21 15:55:34 -0500

    New in people, art, pubs

--- js/pubs.js
+++ js/pubs.js
@@ -29,6 +29,8 @@ var header = document.createElement("h1");
 header.innerHTML = "Selected Publications";
 document.getElementById("publications").appendChild(header);
 
+generatePub("Get.jpg", "Sobiech, T. A.; Zhong, Y. L.; Sánchez B., L. S.; Kauffmann, B.; McGrath, J. K.; Scalzo, C.; Miller, D. P.; Huc, I.; Zurek, E.; Ferrand, Y.; Gong, B.", "Stable pseudo[3]rotaxanes with strong positive binding cooperativity based on shape-persistent aromatic oligoamide macrocycles", "Chem Commun.", "2021", "57", "11645-11648", "https://pubs.rsc.org/en/content/articlelanding/2021/cc/d1cc05193h");
+
 generatePub("2.png", "Wang, Q. H.; Zhong, Y. L.; Miller, D. P.; Lu, X. X.; Tang, Q.; Lu, Z. L.; Zurek, E.; Liu, R.; Gong, B", "Self-assembly and molecular recognition in water: Tubular stacking and guest-templated discrete assembly of water-soluble, shape-persistent macrocycles", "J. Am. Chem. Soc.", "2020", "142", "2915-2924", "https://pubs.acs.org/doi/abs/10.1021/jacs.9b11536");
 
 generatePub("ol0c02696_0008.png", "Cao, R. K.; Rossdeutcher, R. B.; Wu, X. X.; Gong, B", "Oligo(5-amino-N-acylanthranilic acids): Amide Bond Formation without Coupling Reagent and Folding upon Binding Anions", "Org. Lett.", "2020", "22", "7496-7501", "https://pubs.acs.org/doi/10.1021/acs.orglett.0c02696");
